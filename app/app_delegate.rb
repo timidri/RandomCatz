8c373377fa879c0b9ab58849b421d04ec95e74c9
Dimitri Tischenko <dimitri@proteon.nl>
2012-05-13 18:43:40 +0200

    Some cleaning

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,8 +1,5 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
-    # alert = UIAlertView.new
-    # alert.message = "Hello World!"
-    # alert.show
     @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
     @window.rootViewController = LolController.alloc.init
     @window.makeKeyAndVisible
